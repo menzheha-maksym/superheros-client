d6883194e43448ff6fc6c059f4c2cdd4a376e086
Maksym Menzheha <masimfyd@gmail.com>
2022-04-21 00:54:18 +0300

    some hero detail styles

--- src/components/HeroDetails.tsx
+++ src/components/HeroDetails.tsx
@@ -3,11 +3,11 @@ import { useParams } from "react-router-dom";
 import { fetchHeroById } from "../api/heroAPI";
 import { Hero } from "../interfaces/Hero";
 import HeroLastImage from "./HeroLastImage";
-// import styles from "./HeroDetails.module.css";
+import styles from "./HeroDetails.module.css";
 
 interface HeroDetailsProps {}
 
-const HeroDetails: React.FC<HeroDetailsProps> = ({}) => {
+const HeroDetails: React.FC<HeroDetailsProps> = () => {
   const { id } = useParams();
 
   const [hero, setHero] = useState<Hero>();
@@ -22,14 +22,19 @@ const HeroDetails: React.FC<HeroDetailsProps> = ({}) => {
     <>
       <div>
         {hero ? (
-          <div>
-            <HeroLastImage heroId={hero.id} />
-            <div>Nickname: {hero.nickname}</div>
-            <div>Real name: {hero.real_name}</div>
-            <div>Origin description: {hero.origin_description}</div>
-            <div>Superpowers: {hero.superpowers}</div>
-            <div>Catch phrase: {hero.catch_phrase}</div>
-            <div>And images will be here...</div>
+          <div className={styles["details-container"]}>
+            <HeroLastImage
+              heroId={hero.id}
+              imageStyle={styles["latest-image"]}
+            />
+            <div>
+              <div>Nickname: {hero.nickname}</div>
+              <div>Real name: {hero.real_name}</div>
+              <div>Origin description: {hero.origin_description}</div>
+              <div>Superpowers: {hero.superpowers}</div>
+              <div>Catch phrase: {hero.catch_phrase}</div>
+              <div>And images will be here...</div>
+            </div>
           </div>
         ) : null}
       </div>
