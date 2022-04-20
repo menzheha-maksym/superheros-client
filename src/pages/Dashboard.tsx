db4ba881b7dd88f81f4d67824a30ad65449fdf95
Maksym Menzheha <masimfyd@gmail.com>
2022-04-20 20:21:42 +0300

    add pagination

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,16 +2,26 @@ import React, { useEffect, useState } from "react";
 import { fetchHerosWithPagiantion } from "../api/heroAPI";
 import HeroPreview from "../components/HeroPreview";
 import { Hero } from "../interfaces/Hero";
+import Pagination from "../utils/Pagination";
 import styles from "./Dashboard.module.css";
 
 export default function Dashboard() {
+  const limit = 2;
+
   const [heros, setHeros] = useState<Hero[]>();
+  const [itemsCount, setItemsCount] = useState<number>(0);
+  const [skip, setSkip] = useState(0);
 
   useEffect(() => {
-    fetchHerosWithPagiantion().then((res) => {
+    fetchHerosWithPagiantion(limit, skip).then((res) => {
       setHeros(res.data);
+      setItemsCount(res.count);
     });
-  }, []);
+  }, [skip]);
+
+  function updateSkip(skip: number) {
+    setSkip(skip);
+  }
 
   return (
     <div>
@@ -23,6 +33,12 @@ export default function Dashboard() {
           );
         })}
       </div>
+      <Pagination
+        itemsCount={itemsCount}
+        itemsPerPage={limit}
+        updateSkip={updateSkip}
+        skip={skip}
+      />
     </div>
   );
 }
---
+++ src/utils/Pagination.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+interface PaginationProps {
+  itemsCount: number;
+  itemsPerPage: number;
+  updateSkip: (skip: number) => void;
+  skip: number;
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  itemsCount,
+  itemsPerPage,
+  updateSkip,
+  skip,
+}) => {
+  const [disablePrevButton, setDisablePrevButton] = useState(false);
+  const [disableNextButton, setDisableNextButton] = useState(false);
+
+  function previousButtonClick() {
+    updateSkip(skip - itemsPerPage);
+  }
+
+  function nextButtonClick() {
+    updateSkip(skip + itemsPerPage);
+  }
+
+  useEffect(() => {
+    if (skip - itemsPerPage < 0) {
+      setDisablePrevButton(true);
+    } else {
+      setDisablePrevButton(false);
+    }
+
+    if (skip + itemsPerPage >= itemsCount) {
+      setDisableNextButton(true);
+    } else {
+      setDisableNextButton(false);
+    }
+
+    console.log(skip);
+  }, [itemsCount, itemsPerPage, skip]);
+
+  return (
+    <>
+      <button disabled={disablePrevButton} onClick={previousButtonClick}>
+        previous
+      </button>
+      <button disabled={disableNextButton} onClick={nextButtonClick}>
+        next
+      </button>
+    </>
+  );
+};
+
+export default Pagination;
