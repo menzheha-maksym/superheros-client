ea698c454f34f7db773306b74fe5f118e1445e68
Maksym Menzheha <masimfyd@gmail.com>
2022-04-21 21:57:26 +0300

    add add image to hero on hero details page

--- src/pages/HeroDetails/HeroDetails.tsx
+++ src/pages/HeroDetails/HeroDetails.tsx
@@ -1,15 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchHeroById, fetchHeroImagesIds } from "../../api/heroAPI";
+import {
+  fetchHeroById,
+  fetchHeroImagesIds,
+  postHeroImage,
+} from "../../api/heroAPI";
 import { Hero } from "../../interfaces/Hero";
 import HeroImage from "../../components/HeroImage";
 import HeroLastImage from "../../components/HeroLastImage";
 import styles from "./HeroDetails.module.css";
+import React from "react";
 
 interface HeroDetailsProps {}
 
 const HeroDetails: React.FC<HeroDetailsProps> = () => {
   const { id } = useParams();
+
+  const fileInputRef = useRef<HTMLInputElement>();
 
   const [hero, setHero] = useState<Hero>();
   const [imageIds, setImageIds] = useState<number[]>();
@@ -25,6 +32,25 @@ const HeroDetails: React.FC<HeroDetailsProps> = () => {
       setImageIds(ids);
     });
   }, [id]);
+
+  async function handleAddImageToHero(e: any) {
+    try {
+      if (hero) {
+        const data = new FormData();
+        data.append("heroId", String(hero.id));
+        data.append("file", e.target.files[0]);
+        await postHeroImage(data).then((id) => {
+          //console.log(id);
+          // must add image id to ids
+          // but id is not being returned for unknown reason
+          // eslint-disable-next-line no-restricted-globals
+          location.reload();
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <>
@@ -55,6 +81,20 @@ const HeroDetails: React.FC<HeroDetailsProps> = () => {
           </div>
         ) : null}
         <h2>Hero images</h2>
+        <button
+          className={styles["add-image-to-hero-button"]}
+          onClick={() => fileInputRef.current!.click()}
+        >
+          Add an image to hero
+        </button>
+
+        <input
+          type="file"
+          // @ts-ignore wasting of time
+          ref={fileInputRef}
+          onChange={handleAddImageToHero}
+          hidden
+        />
         <div className={styles["hero-images-container"]}>
           {imageIds
             ? imageIds.map((id, i) => {
