484cd25b31b3c17f8b7de90c0945934145060120
Maksym Menzheha <masimfyd@gmail.com>
2022-04-21 15:07:44 +0300

    add here create form

--- src/pages/AddHero/AddHero.tsx
+++ src/pages/AddHero/AddHero.tsx
@@ -1,5 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./AddHero.module.css";
 
 export default function AddHero() {
-  return <div>AddHero</div>;
+  const [nickname, setNickname] = useState<string>("");
+  const [real_name, setReal_name] = useState<string>("");
+  const [origin_description, setOrigin_description] = useState<string>("");
+  const [superpowers, setSuperpowers] = useState<string>("");
+  const [catch_phrase, setCatch_phrase] = useState<string>("");
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+
+    console.log(nickname);
+    console.log(real_name);
+    console.log(origin_description);
+    console.log(superpowers);
+    console.log(catch_phrase);
+  }
+
+  return (
+    <div className={styles["container"]}>
+      <form onSubmit={handleSubmit} className={styles["form"]}>
+        <span>Nickname</span>
+        <input
+          type="text"
+          name="nickname"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+        <span>Real name</span>
+        <input
+          type="text"
+          name="real_name"
+          value={real_name}
+          onChange={(e) => setReal_name(e.target.value)}
+        />
+        <span>Origin description</span>
+        <input
+          type="text"
+          name="origin_description"
+          value={origin_description}
+          onChange={(e) => setOrigin_description(e.target.value)}
+        />
+        <span>Superpowers</span>
+        <input
+          type="text"
+          name="superpowers"
+          value={superpowers}
+          onChange={(e) => setSuperpowers(e.target.value)}
+        />
+        <span>Catch Phrase</span>
+        <input
+          type="text"
+          name="superpowers"
+          value={catch_phrase}
+          onChange={(e) => setCatch_phrase(e.target.value)}
+        />
+
+        <button type="submit">Add a hero</button>
+      </form>
+    </div>
+  );
 }
