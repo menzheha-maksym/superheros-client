1f90e35483cf00fbfcbd8d28d3edf8a5a6e2606c
Maksym Menzheha <masimfyd@gmail.com>
2022-04-22 14:44:19 +0300

    edit hero button

--- src/components/heroDescription/HeroDescription.tsx
+++ src/components/heroDescription/HeroDescription.tsx
@@ -6,23 +6,30 @@ interface HeroDescriptionProps {
 }
 
 const HeroDescription: React.FC<HeroDescriptionProps> = ({ hero }) => {
+  function handleEditHero() {}
+
   return (
-    <div className={styles["text-description"]}>
-      <div>
-        <span>Nickname:</span> {hero.nickname}
+    <div>
+      <div className={styles["text-description"]}>
+        <div>
+          <span>Nickname:</span> {hero.nickname}
+        </div>
+        <div>
+          <span>Real name:</span> {hero.real_name}
+        </div>
+        <div>
+          <span>Origin description:</span> {hero.origin_description}
+        </div>
+        <div>
+          <span>Superpowers:</span> {hero.superpowers}
+        </div>
+        <div>
+          <span>Catch phrase:</span> {hero.catch_phrase}
+        </div>
       </div>
-      <div>
-        <span>Real name:</span> {hero.real_name}
-      </div>
-      <div>
-        <span>Origin description:</span> {hero.origin_description}
-      </div>
-      <div>
-        <span>Superpowers:</span> {hero.superpowers}
-      </div>
-      <div>
-        <span>Catch phrase:</span> {hero.catch_phrase}
-      </div>
+      <button className={styles["edit-hero-button"]} onClick={handleEditHero}>
+        Edit hero
+      </button>
     </div>
   );
 };
