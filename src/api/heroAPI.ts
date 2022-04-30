d57fdf5ea298797a7d5384d7a2420c191c0d15b7
Maksym Menzheha <masimfyd@gmail.com>
2022-04-30 16:34:37 +0300

    add delete hero image api request

--- src/api/heroAPI.ts
+++ src/api/heroAPI.ts
@@ -102,3 +102,16 @@ export function updateHero(hero: Partial<Hero>) {
       .catch((err) => reject(err));
   });
 }
+
+export function deleteHeroImage(id: number) {
+  return new Promise(async (resolve, reject) => {
+    await fetch(`http://localhost:4000/hero-image/delete`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    })
+      .then((res) => res.json())
+      .then((json) => resolve(json))
+      .catch((err) => reject(err));
+  });
+}
